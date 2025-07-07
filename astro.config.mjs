a93398a52d0b669b455d4e9f217ddb7959125b15
Cameron Steele <cam.steeleis@gmail.com>
2025-07-06 23:19:38 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,16 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://austin-mesh-astro.pages.dev",
   adapter: cloudflare(),
+
   redirects: {
     "/solar": "/devices#solar",
   },
+
+  integrations: [sitemap()],
 });
